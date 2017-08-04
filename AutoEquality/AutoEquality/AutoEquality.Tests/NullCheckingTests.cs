2abad4721952fb5d8f2bdd02d38edd2e1e3cd5ce
whortleberrybearer <30476438+whortleberrybearer@users.noreply.github.com>
2017-08-04 16:38:16 +0100

    Null handling (#4)
    * Got null object test.
    
    * Added null check on with and without parameters.

---
+++ AutoEquality/AutoEquality/AutoEquality.Tests/NullCheckingTests.cs
@@ -0,0 +1,40 @@
+﻿namespace AutoEquality.Tests
+{
+    using AutoEquality.Tests.HelperClasses;
+    using Ploeh.AutoFixture.Xunit2;
+    using Shouldly;
+    using Xunit;
+
+    public class NullCheckingTests
+    {
+        [Theory]
+        [InlineAutoData]
+        public void BothNullShouldBeTrue(AutoEqualityComparer<MultiPropertyClass> sut)
+        {
+            var result = sut.Equals(null, null);
+
+            result.ShouldBeTrue();
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public void NonMatchingNullPropertyShouldBeFalse(string value1, string value2, string value3, AutoEqualityComparer<MultiPropertyClass> sut)
+        {
+            var simpleClass1 = new MultiPropertyClass() { Property1 = value1, Property2 = value2, Property3 = value3 };
+            var simpleClass2 = new MultiPropertyClass() { Property1 = value1, Property2 = null, Property3 = value3 };
+
+            var result = sut.Equals(simpleClass1, simpleClass2);
+
+            result.ShouldBeFalse();
+        }
+
+        [Theory]
+        [InlineAutoData]
+        public void NullItemShouldBeFalse(MultiPropertyClass multiPropertyClass, AutoEqualityComparer<MultiPropertyClass> sut)
+        {
+            var result = sut.Equals(multiPropertyClass, null);
+
+            result.ShouldBeFalse();
+        }
+    }
+}
--- AutoEquality/AutoEquality/AutoEquality.Tests/SimpleClassTests.cs
+++ AutoEquality/AutoEquality/AutoEquality.Tests/SimpleClassTests.cs
@@ -1,5 +1,8 @@
 ﻿namespace AutoEquality.Tests
 {
+    using System;
+    using System.Linq.Expressions;
+    using AutoEquality.Tests.HelperClasses;
     using Ploeh.AutoFixture.Xunit2;
     using Shouldly;
     using Xunit;
@@ -32,15 +35,6 @@
                 // The default construction sets up all properties, so a non match will fail.
                 result.ShouldBeFalse();
             }
-        }
-
-        public class MultiPropertyClass
-        {
-            public string Property1 { get; set; }
-
-            public string Property2 { get; set; }
-
-            public string Property3 { get; set; }
         }
 
         public class WithAllTests
@@ -124,6 +118,13 @@
 
             [Theory]
             [InlineAutoData]
+            public void NullPropertyShouldThrowArgumentNullException(AutoEqualityComparer<MultiPropertyClass> sut)
+            {
+                Should.Throw<ArgumentNullException>(() => sut.With((Expression<Func<MultiPropertyClass, object>>)null));
+            }
+
+            [Theory]
+            [InlineAutoData]
             public void SingleValueShouldNotBeCompared(string value1, string value2, string value3, AutoEqualityComparer<MultiPropertyClass> sut)
             {
                 var simpleClass1 = new MultiPropertyClass() { Property1 = value1, Property2 = value2.ToLower(), Property3 = value3 };
@@ -153,6 +154,13 @@
                 var result = sut.Equals(simpleClass1, simpleClass2);
 
                 result.ShouldBeTrue();
+            }
+
+            [Theory]
+            [InlineAutoData]
+            public void NullPropertyShouldThrowArgumentNullException(AutoEqualityComparer<MultiPropertyClass> sut)
+            {
+                Should.Throw<ArgumentNullException>(() => sut.With((Expression<Func<MultiPropertyClass, object>>)null));
             }
 
             [Theory]
