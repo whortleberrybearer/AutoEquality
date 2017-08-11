4e63eea47d637e5160b341bdc8bd9c6703584961
Barry Wheeler <Barry.Wheeler3@amey.co.uk>
2017-08-11 16:29:53 +0100

    Creation of unit test.

---
+++ AutoEquality/AutoEquality/AutoEquality.Tests/TypeComparerTests.cs
@@ -0,0 +1,44 @@
+﻿namespace AutoEquality.Tests
+{
+    using AutoEquality.Tests.HelperClasses;
+    using Ploeh.AutoFixture.Xunit2;
+    using Shouldly;
+    using Xunit;
+
+    public class TypeComparerTests
+    {
+        [Theory]
+        [InlineAutoData]
+        public void MatchingTypeShouldBeTrue(string value1, string value2, string value3, string value4, AutoEqualityComparer<DeepClass2> typeComparer, AutoEqualityComparer<DeepClass1> sut)
+        {
+            var deepClass1 = new DeepClass1()
+            {
+                Property1 = value1,
+                Property2 = value2,
+                Deep = new DeepClass2()
+                {
+                    Property3 = value3,
+                    Property4 = value4.ToUpper()
+                }
+            };
+            var deepClass2 = new DeepClass1()
+            {
+                Property1 = value1,
+                Property2 = value2,
+                Deep = new DeepClass2()
+                {
+                    Property3 = value3,
+                    Property4 = value4.ToLower()
+                }
+            };
+
+            typeComparer.Without(a => a.Property4);
+
+            sut.WithComparer(typeComparer);
+
+            var result = sut.Equals(deepClass1, deepClass2);
+
+            result.ShouldBeTrue();
+        }
+    }
+}
